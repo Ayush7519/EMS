754e97e29c107d74f145e8005a73855f7d6bfdcd
aayush <ayushkandel7519@gmail.com>
2023-06-14 21:11:00 +0545

    change in the setting of the email configurations

--- ems/settings.py
+++ ems/settings.py
@@ -147,8 +147,10 @@ AUTH_USER_MODEL = "account.User"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
+# EMAIL_HOST_USER = os.environ.get("EMAIL_USER")
+EMAIL_HOST_USER = "ayushkandel7519@gmail.com"
+# EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASS")
+EMAIL_HOST_PASSWORD = "ueayslphrkhpfahl"
 EMAIL_USE_TLS = True
 
 # media url for the image path
